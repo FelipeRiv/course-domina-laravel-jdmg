be63c8defb6233f611af6fb981147f169b422347
FelipeRiv <luisriv_@outlook.com>
2022-10-30 16:46:20 -0600

    63 usando e insertando relaciones simples de modelos en el seeder de laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\Payment;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,5 +20,21 @@ class DatabaseSeeder extends Seeder
         // ## Seeders ejecutan lo que definimos en los Factories 
         // \App\Models\User::factory(10)->create();
         $products = Product::factory(50)->create();
+        
+        $users = User::factory(20)->create();
+        
+        $orders = Order::factory(10)
+            ->make()
+            ->each(function ($order) use ($users) {
+                $order->customer_id = $users->random()->id;
+                $order->save();
+
+                $payment = Payment::factory()->make();
+
+                // $payment->order_id = $order->id;
+                // $payment->save();
+
+                $order->payment()->save($payment);
+            });
     }
 }
